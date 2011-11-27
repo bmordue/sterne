d3a84851254afe06e9aaf3003d9857fa712c7017
Ben Mordue <benmordue@gmail.com>
2011-11-27 12:41:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sterne::Application.config.secret_token = 'e80e0d5e00bd7621fd20f586e83b418adf9f427661a3aba8f92c9d732aa452a50026eb9ea7d2259a002c28259d302f6db8c7c7c3619cdb2ad11dcfdeaa269d2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sterne::Application.config.session_store :cookie_store, :key => '_sterne_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sterne::Application.config.session_store :active_record_store
