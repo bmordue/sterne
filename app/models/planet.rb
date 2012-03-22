6dde902da7510177cc4997679745710915b033f4
ben <benmordue@gmail.com>
2012-03-22 19:19:31 +0000

    Added requirements to Planet model.

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,11 @@
 class Planet < ActiveRecord::Base
+   validates_numericality_of :locationX, :allow_nil => false
+   validates_numericality_of :locationY, :allow_nil => false
+   
+   validates_numericality_of :manufacturing
+   validates_numericality_of :growth
+   validates_numericality_of :production
+   validates_numericality_of :research
+   validates_numericality_of :science
+
 end
