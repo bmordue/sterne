e66d6da72d0c4cc6c13477e566dbcfdd4f511d4f
ben <benmordue@gmail.com>
2012-03-22 19:15:34 +0000

    Add requirements to Ship model.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,12 @@
 class Ship < ActiveRecord::Base
+   validates_presence_of :player_id #TODO: should validate that player_id is a valid player
+ 
+
+   validates_numericality_of :locationX, :allow_nil => false
+   validates_numericality_of :locationY, :allow_nil => false
+   validates_numericality_of :hitpoints, :allow_nil => false
+   
+   #TODO: validate planet id in destination   
+   
+   validates_presence_of :shiptype_id #TODO: should validate for shiptype
 end
