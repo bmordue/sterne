2490a16354ce46ffaf7c002a20d3d717ba377f30
ben <benmordue@gmail.com>
2012-03-28 17:58:09 +0100

    Minor correction to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,5 @@ class Game < ActiveRecord::Base
    validates_numericality_of :universe_id #TODO: validate existing universe_id
    has_many :players
    has_one :universe
-  has_many :players
-  
-  has_one :universe
 
 end
